2105180c366d4a0e3d49a7df949742f9e41c2336
rubab-rafiq <rubabrafique05@gmail.com>
2024-12-18 10:42:31 -0800

    Fixed image path and updated Contact page

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
