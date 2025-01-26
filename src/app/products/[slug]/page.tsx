9b4266e08f17fa3773ec99c08f176accbf4aeb6b
rubab-rafiq <rubabrafique05@gmail.com>
2025-01-26 02:37:22 -0800

    error fixed

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -40,28 +40,69 @@
 
 
 
+// import { IProduct } from "@/types/page";
+// import { client } from "@/sanity/lib/client";
+// import { urlFor } from "@/sanity/lib/image";
+// import Navbar from "@/app/Components/navbar2";
+// import ProductDetails from "@/app/ProductDetails/page";
+// import Footer from "@/app/Components/footer";
+
+// interface PageProps {
+//   params: {
+//     slug: string;
+//   };
+// }
+
+// export default async function Page({ params }: PageProps) {
+//   const { slug } = params;
+
+//   // Sanity query to fetch product based on the slug
+//   const query = `*[_type=='product' && slug.current=="${slug}"]{
+//     title, image, price, discountPrice, slug, description, colors, sizes, quantity
+//   }[0]`;
+
+//   const product = await client.fetch(query);
+
+//   if (!product) {
+//     return (
+//       <div className="text-center text-red-500 font-extrabold text-4xl p-10">
+//         <h2>Product not found</h2>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <>
+//       <Navbar />
+//       <ProductDetails product={product} />
+//       <Footer />
+//     </>
+//   );
+// }
 import { IProduct } from "@/types/page";
-import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
-import Navbar from "@/app/Components/navbar2";
+import Image from "next/image";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import ProductDetails from "@/app/ProductDetails/page";
 import Footer from "@/app/Components/footer";
-
+import Navbar from "@/app/Components/navbar2";
+import { client } from "@/sanity/lib/client";
 interface PageProps {
   params: {
     slug: string;
   };
 }
 
-export default async function Page({ params }: PageProps) {
+const Page = async ({ params }: PageProps) => {
   const { slug } = params;
 
-  // Sanity query to fetch product based on the slug
+  // Sanity query
   const query = `*[_type=='product' && slug.current=="${slug}"]{
     title, image, price, discountPrice, slug, description, colors, sizes, quantity
   }[0]`;
 
-  const product = await client.fetch(query);
+  const product: IProduct = await client.fetch(query);
 
   if (!product) {
     return (
@@ -78,4 +119,6 @@ export default async function Page({ params }: PageProps) {
       <Footer />
     </>
   );
-}
+};
+
+export default Page;
