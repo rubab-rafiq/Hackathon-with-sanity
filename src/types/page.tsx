7494177a1a95a8f056631eb870149897b0f3254c
rubab-rafiq <rubabrafique05@gmail.com>
2025-02-02 00:54:07 +0500

    ..

--- src/types/page.tsx
+++ src/types/page.tsx
@@ -2,7 +2,7 @@
 export default interface IProduct {
   imageUrl: string;
   title: string;
-  slug: string | any;
+  slug: string ;
   price: number;
   discountPrice: number;
   description: string;
