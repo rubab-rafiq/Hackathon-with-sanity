fa4ec06aaa80f3dabf35908e78b72ee3d16d46c5
rubab-rafiq <rubabrafique05@gmail.com>
2025-02-03 20:09:45 +0500

    updated

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,9 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  projectId:'mcvob9lf',
+  dataset:'production',
+
+ token:'sk3YSJuAPMK4ROAdXjAEo7e74eNFoCTE613ldlDP6ErhCSc51bnRS9lAzVODqPDsskWhYwf0a1Q0gB0zXYNz0JR3uGj1EoJBpWLx2QidBMN0tVs0CmkMVvUzzbttKo0ZTBG2ION03MzWnF15ENGfAbJEDSEU7KFZQkOCqXyeF6MyLYbn8yc7',
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
